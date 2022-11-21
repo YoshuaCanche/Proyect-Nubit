5e321c995084c82b9f77f79b097cdead69e23b37
David R. Ake Prott <davidake117@gmail.com>
2022-11-21 13:46:17 -0600

    solucion a los de Yoahua!, insert datos laborales

--- Programación Web Nubit/Controllers/DatosLaboralescontroller.cs
+++ Programación Web Nubit/Controllers/DatosLaboralescontroller.cs
@@ -6,51 +6,40 @@ using System.Threading.Tasks;
 
 namespace Programación_Web_Nubit.Controllers
 {
-    public class Datos_laboralescontroller : Controller
-    {
-
-        private readonly ILogger<HomeController> _logger;
+	public class DatosLaboralesController : Controller
+	{
+        
         private readonly ApplicationDbContext _context;
-
-        public Datos_laboralescontroller(ILogger<HomeController> logger, ApplicationDbContext context)
-        {
-            _logger = logger;
+        private readonly ILogger<HomeController> _logger;
+        public DatosLaboralesController(ILogger<HomeController> logger, ApplicationDbContext context)
+        {           
             _context = context;
+            _logger = logger;
         }
 
-        public IActionResult Index()
+        public IActionResult Crear()
         {
             return View();
         }
 
         [HttpPost]
-        public async Task<ActionResult> CrearDatos_laborales(Datos_laborales request)
+        public async Task<IActionResult> CrearDato_laboral(Datos_laborales request)
         {
             if (request != null)
             {
                 Datos_laborales datos_laborales = new Datos_laborales();
 
-               datos_laborales.Nombrempresa = request.Nombrempresa;
+                datos_laborales.Nombrempresa = request.Nombrempresa;
                 datos_laborales.lugartrabajo = request.lugartrabajo;
                 datos_laborales.puestoempeñado = request.puestoempeñado;
                 datos_laborales.tiempoprestado = request.tiempoprestado;
 
-               _context.Datos_laborales.Add(datos_laborales);
+                _context.Datos_laborales.Add(datos_laborales);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Crear));
             }
             return View();
         }
 
-        [HttpGet]
-        public IActionResult Crear()
-        {
-            return View();
-        }
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-        
     }
 }
