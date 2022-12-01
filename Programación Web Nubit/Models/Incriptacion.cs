3726b38308acbe96d9d51c1a06b2602087ab7def
David R. Ake Prott <davidake117@gmail.com>
2022-11-30 19:19:39 -0600

    Creacion Clase Incriptacion

---
+++ Programación Web Nubit/Models/Incriptacion.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text; 
+
+namespace Programación_Web_Nubit.Models
+{
+    public class Incriptacion
+    {
+
+        public string encriptar(string mensaje)
+        {
+            string hash = "Empresa Nubit";
+            byte[] data = UTF8Encoding.UTF8.GetBytes(mensaje);
+
+            MD5 md5 = MD5.Create();
+            TripleDES tripides = TripleDES.Create();
+
+            tripides.Key = md5.ComputeHash(UTF8Encoding.UTF8.GetBytes(hash));
+            tripides.Mode = CipherMode.ECB;
+
+            ICryptoTransform transform = tripides.CreateEncryptor();
+            byte[] result = transform.TransformFinalBlock(data, 0, data.Length);
+
+            return Convert.ToBase64String(result);
+        }
+
+        public string desencriptar(string mensajeEn)
+        {
+            string hash = "Empresa Nubit";
+            byte[] data = Convert.FromBase64String(mensajeEn);
+
+            MD5 md5 = MD5.Create();
+            TripleDES tripides = TripleDES.Create();
+
+            tripides.Key = md5.ComputeHash(UTF8Encoding.UTF8.GetBytes(hash));
+            tripides.Mode = CipherMode.ECB;
+
+            ICryptoTransform transform = tripides.CreateDecryptor();
+            byte[] result = transform.TransformFinalBlock(data, 0, data.Length);
+
+            return UTF8Encoding.UTF8.GetString(result);
+        }
+    }
+}
